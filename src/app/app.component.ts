bf15edd36c537c0e4bb2108fee58b116d13ec4e5
Ryo Suzuki <ryo.s12330@gmail.com>
2019-06-16 23:39:05 +0900

    双方向データバインディング

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <input #txt id="txt" name="txt" type="text" (keyup.enter)="show(txt.value)" />
-  <ul [innerHTML]="msg"></ul>
-
-  <label>苗字：<input  #last  type="text" (change)="0" /></label><br />
-  <label>名前：<input  #first type="text" (change)="0" /></label>
-  <div>こんにちは、{{last.value}}{{first.value}}さん！</div>
+  <form>
+  	<label for="name">名前：</label>
+  	<input id="name"  name="name"  type="text"  [(ngModel)]="myName" />
+  	<div>こんにちは、{{myName}}さん！</div>
+  </form>
   `
 })
 export class AppComponent  {
-	msg = '';
-
-	show(input: any){
-		this.msg += `<li>${input}</li>`;
-	}
+	myName = '山田';
 }
