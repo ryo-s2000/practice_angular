213627f714358511861d005244004af3d7fe1001
Ryo Suzuki <ryo.s12330@gmail.com>
2019-06-16 22:26:40 +0900

    キー入力

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<div class="line back" [class.fore]="flag">WINGSプロジェクト</div>`,
-  styles: [`
-  	.line { border: solid 1px #f00; }
-  	.back { background-color: #0ff; }
-  	.fore { color: red; }
-  `]
+  template: `
+  <form>
+  	<label for="key">キー入力：</label>
+  	<input id="key" name="key" (keydown)="show($event)" />
+  </form>
+  <div>キーコード：{{which}}</div>
+  <div [hidden]="!altKey">[Alt]</div>
+  <div [hidden]="!ctrlKey">[Ctrl]</div>
+  <div [hidden]="!shiftKey">[Shift]</div>
+  `
 })
 export class AppComponent  {
-	flag = true;
-	clazz = 'fore';
+	which = '';
+	altKey = false;
+	ctrKey = false;
+	shiftKey = false;
+
+	show(e: any){
+		this.which = e.which;
+		this.altKey = e.altKey;
+		this.ctrlKey = e.ctrlKey;
+		this.shiftKey = e.shiftKey;
+	}
 }
