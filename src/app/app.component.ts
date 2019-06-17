de4357f48d7f9cd07fab73f91dbd0f364e7477cf
Ryo Suzuki <ryo.s12330@gmail.com>
2019-06-18 07:15:52 +0900

    表示、非表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <ul>
-  	<li>{{current | date }}</li>
-  	<li>{{current | date : "medium" }}</li>
-  	<li>{{current | date : "y MM dd (EEE) "}}</li>
-  </ul>
+  <form>
+  	<label for="show">表示：非表示</label>
+  	<input id="show" name="show" type="checkbox" [(ngModel)] = "show" />
+  </form>
+  <div *ngIf="show; else elseContent">
+  	<p>hogehoge</p>
+  </div>
+  <ng-template #elseContent>
+  	<h3 style="color:Red;">非表示中だよ</h3>
+  </ng-template>
   `
 })
 export class AppComponent  {
-	current = new Date()
+	show = false;
 }
