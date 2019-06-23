27f3f41af4e75add2359f7f718ab421ee8330595
Ryo Suzuki <ryo.s12330@gmail.com>
2019-06-23 21:24:52 +0900

    コンポーネント間の情報通信

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,37 @@
-import { Component, OnChanges, OnInit, DoCheck,
-  AfterContentInit, AfterContentChecked, 
-  AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { Component, AfterViewChecked, QueryList, ViewChildren } from '@angular/core';
+import { ChildComponent } from './child.component';
 
 @Component({
   selector: 'my-app',
   template: `
-    <div>
-      <label>表示／非表示
-        <input type="checkbox" (change)="onchange()" checked />
-      </label>
-    </div>
-    <my-child [time]="current" *ngIf="show"></my-child>
+    <my-child [index]="1"></my-child>
+    <my-child [index]="2"></my-child>
+    <my-child [index]="3"></my-child>
+    <hr />
+    完成：{{ poems[0] }} {{ poems[1] }} {{ poems[2] }}
   `,
 })
-export class AppComponent implements OnChanges,
-  OnInit, DoCheck, AfterContentInit, AfterContentChecked,
-  AfterViewInit, AfterViewChecked, OnDestroy {
+export class AppComponent implements AfterViewChecked{
+  @ViewChildren(ChildComponent) children: QueryList<ChildComponent>;
+  poems = ['', '', ''];
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked");
+
+    this.children.forEach((item, index) => {
+      if(this.poems[index] !== item.poem){
+        setTimeout(() => {
+          this.poems[index] = item.poem;
+        }, 0);
+      }
+    });
+  }
+
   show = true;
   current = new Date();
 
   onchange() {
     this.show = !this.show;
     this.current = new Date();
-  }
-
-  constructor() {
-    console.log('constructor');
-   }
-
-  ngOnInit() {
-    console.log('ngOnInit');
-  }
-
-  ngOnChanges() {
-    console.log('ngOnChanges');
-  }
-
-  ngDoCheck() {
-    console.log('ngDoCheck');
-  }
-
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit');
-  }
-
-  ngAfterContentChecked() {
-    console.log('ngAfterContentChecked');
-  }
-
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit');
-  }
-
-  ngAfterViewChecked() {
-    console.log('ngAfterViewChecked');
-  }
-
-  ngOnDestroy() {
-    console.log('ngOnDestroy');
   }
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,59 +1,15 @@
-import { Component, Input, OnChanges, OnInit, DoCheck,
-    AfterContentInit, AfterContentChecked, 
-    AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges
-  } from '@angular/core';
+import { Component, Input } from '@angular/core';
   
   @Component({
     selector: 'my-child',
-    template: `<div>現在時刻は{{time.toLocaleString()}}</div>`,
+    template: `
+    <div>
+        三行詩{{index}}:<input name="poem" [(ngModel)]="poem" size="20" />
+    </div>
+    `,
   })
-  export class ChildComponent implements OnChanges,
-    OnInit, DoCheck, AfterContentInit, AfterContentChecked,
-    AfterViewInit, AfterViewChecked, OnDestroy {
-     @Input() time: Date; 
-  
-     constructor() {
-      console.log('［child］constructor');
-     }
-  
-    ngOnInit() {
-      console.log('［child］ngOnInit');
-    }
-  
-    ngOnChanges() {
-      console.log('［child］ngOnChanges');
-    }
-    /*
-    ngOnChanges(changes: SimpleChanges) {
-      console.log('［child］ngOnChanges');
-      for (let prop in changes) {
-        let change = changes[prop];
-        console.log(`${prop}：${change.previousValue} => ${change.currentValue}`);
-      }
-    }
-    */
-  
-    ngDoCheck() {
-      console.log('［child］ngDoCheck');
-    }
-  
-    ngAfterContentInit() {
-      console.log('［child］ngAfterContentInit');
-    }
-  
-    ngAfterContentChecked() {
-      console.log('［child］ngAfterContentChecked');
-    }
-  
-    ngAfterViewInit() {
-      console.log('［child］ngAfterViewInit');
-    }
-  
-    ngAfterViewChecked() {
-      console.log('［child］ngAfterViewChecked');
-    }
-  
-    ngOnDestroy() {
-      console.log('［child］ngOnDestroy');
-    }
+  export class ChildComponent{
+     @Input() index: number;
+
+     poem: string;
   }
